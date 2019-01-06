a6156479fb1eb07868ce578d697727730f1675c8
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-06 17:13:40 -0300

    Initial commit

---
+++ app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.fffrowies.broadcastreceiverdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendBroadcastMessage(View view) {
+    }
+}
