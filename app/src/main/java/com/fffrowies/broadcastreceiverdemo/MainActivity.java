d2934eee6754959d33c0948732865f9827664aba
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-06 17:32:24 -0300

    First broadcast receiver

--- app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fffrowies.broadcastreceiverdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendBroadcastMessage(View view) {
+        Intent intent = new Intent(this, MyFirstReceiver.class);
+        sendBroadcast(intent);
     }
 }
---
+++ app/src/main/java/com/fffrowies/broadcastreceiverdemo/MyFirstReceiver.java
@@ -0,0 +1,20 @@
+package com.fffrowies.broadcastreceiverdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyFirstReceiver extends BroadcastReceiver {
+
+    private static final String TAG = MyFirstReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.i(TAG, "Hello from 1st Receiver");
+        Toast.makeText(context, "Hello from 1st Receiver", Toast.LENGTH_LONG).show();
+
+    }
+}
