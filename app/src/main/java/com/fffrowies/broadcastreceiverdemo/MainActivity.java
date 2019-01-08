f214ed32435e580efda641dda3f3a356f10f6a57
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-08 09:30:30 -0300

    Using dynamic receiver

--- app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
@@ -1,15 +1,22 @@
 package com.fffrowies.broadcastreceiverdemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private MyFirstReceiver myFirstReceiver;
+    private TextView textView;
+    private int ctr = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myFirstReceiver = new MyFirstReceiver();
+        textView = findViewById(R.id.textView);
+
     }
 
     @Override
@@ -36,4 +45,30 @@ public class MainActivity extends AppCompatActivity {
 
         unregisterReceiver(myFirstReceiver);
     }
+
+    public void registerReceiver(View view) {
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_TIME_TICK);
+
+        registerReceiver(timeTickReceiver, intentFilter);
+    }
+
+    public void unregisterReceiver(View view) {
+
+        unregisterReceiver(timeTickReceiver);
+
+    }
+
+    private BroadcastReceiver timeTickReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int minutes = ctr;
+            textView.setText(minutes + " minutes over");
+            ctr++;
+
+            Toast.makeText(context, "Hello from TimeTick Receiver", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
