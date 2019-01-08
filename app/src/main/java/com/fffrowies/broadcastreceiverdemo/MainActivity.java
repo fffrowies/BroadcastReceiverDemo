b3f7f536e74121b7af0e3e5797f1bf1d5cb00063
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-07 22:39:22 -0300

    Dynamically created and registered BroadcastReceiver

--- app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/fffrowies/broadcastreceiverdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.fffrowies.broadcastreceiverdemo;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,31 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private MyFirstReceiver myFirstReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myFirstReceiver = new MyFirstReceiver();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+        // intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+
+        registerReceiver(myFirstReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(myFirstReceiver);
     }
 }
